5425335d9f81a45c34adb7fba0e30742ed5af668
npiper <n_piper@hotmail.com>
2018-04-28 17:20:36 +0000

    Updated

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 git config --global user.name "${GIT_USER_NAME}"
 git config --global user.email "${GIT_USER_EMAIL}"
 mvn clean install deploy scm:tag -Drevision=$(git rev-parse --short HEAD) -Dusername=${GIT_USER_NAME} -Dpassword=${GITPW}
