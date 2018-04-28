bce9afc0214746b329d430fe7025f81ac5da5dff
npiper <n_piper@hotmail.com>
2018-04-28 17:16:42 +0000

    Updated push

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git config --global user.name "${GIT_USER_NAME}"
 git config --global user.email "${GIT_USER_EMAIL}"
-cp .travis.settings.xml $HOME/.m2/settings.xml && git config -l &&  mvn clean install deploy scm:tag -Drevision=$(git rev-parse --short HEAD)
+mvn clean install deploy scm:tag -Drevision=$(git rev-parse --short HEAD) -Dusername=${GIT_USER_NAME} -Dpassword=${GITPW}
